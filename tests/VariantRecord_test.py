26da8ef6c0830d5b01f40be7b0c557c4b3bdb0a4
git@buysdb.nl <git@buysdb.nl>
2022-07-26 13:58:01 +0200

    Added test cases

--- tests/VariantRecord_test.py
+++ tests/VariantRecord_test.py
@@ -52,7 +52,7 @@ def test_ascii_annotation_with_variable_length_can_be_added(vcf_header):
             {"AN": "anno2"},
             {"AN": "anno2b"}])
     assert str(record)[:-1].split("\t")[-2:] == ["anno2", "anno2b"]
-    
+
 
 def test_unicode_annotation_can_be_added(vcf_header):
     vcf_header.formats.add("AN", 1, "String", "An annotation")
@@ -66,6 +66,7 @@ def test_unicode_annotation_can_be_added(vcf_header):
     assert str(record)[:-1].split("\t")[-2:] == [
         "anno1",
         "Friedrich-Alexander-Universit\u00E4t_Erlangen-N\u00FCrnberg"]
+
 
 def test_set_sample_alleles(vcf_header):
     vcf_header.formats.add('GT',1,'String',"Genotype") # id, number, type, description
@@ -93,3 +94,47 @@ def test_set_sample_alleles(vcf_header):
 
     with pytest.raises(ValueError, match='Use .allele_indices to set integer allele indices'):
         record.samples['sample1'].alleles = (1, 0)
+
+
+    # Multi allelic test:
+    record_B = vcf_header.new_record(
+        contig="1",
+        start=21,
+        stop=22,
+        alleles=('C','A','G')
+        )
+
+    # Two alternative alleles
+    record_B.samples['sample1'].alleles = ('A', 'G')
+    assert record_B.samples['sample1'].alleles  == ('A', 'G')
+
+    # Tri-allelic site
+    record_B.samples['sample1'].alleles = ('G', 'A', 'G')
+    assert record_B.samples['sample1'].alleles  == ('G', 'A', 'G')
+
+
+def test_sample_properties(vcf_header):
+
+    vcf_header.formats.add('GT',1,'String',"Genotype") # id, number, type, description
+    record = vcf_header.new_record(
+        contig="1",
+        start=20,
+        stop=21,
+        alleles=('A','T')
+        )
+
+    record.samples['sample1'].alleles = ('T', 'A')
+    assert record.samples['sample1'].is_homozygous==False
+    assert record.samples['sample1'].is_heterozygous==True
+
+    record.samples['sample1'].alleles = ('A', 'A')
+    assert record.samples['sample1'].is_homozygous==True
+    assert record.samples['sample1'].is_heterozygous==False
+
+    record.samples['sample1'].alleles = ('A', )
+    assert record.samples['sample1'].is_homozygous==False
+    assert record.samples['sample1'].is_heterozygous==False
+
+    record.samples['sample1'].alleles = ('T', )
+    assert record.samples['sample1'].is_homozygous==False
+    assert record.samples['sample1'].is_heterozygous==False
